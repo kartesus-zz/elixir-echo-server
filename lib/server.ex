b1b279b6e50339b73a42700045901b53de3efaf8
Alexandre Gravem <gravem@gmail.com>
2014-02-01 19:16:00 +0000

    renames server function

--- lib/server.ex
+++ lib/server.ex
@@ -1,23 +1,21 @@
 defmodule Echo.Server do
   def start(port) do
-    tcp_options = 
-      [:list, {:packet, 0}, {:active, false}, {:reuseaddr, true}]
-
+    tcp_options = [:binary, {:packet, 0}, {:active, false}]
     {:ok, socket} = :gen_tcp.listen(port, tcp_options)
     listen(socket)
   end
 
   defp listen(socket) do
     {:ok, conn} = :gen_tcp.accept(socket)
-    spawn(fn -> server(conn) end)
+    spawn(fn -> recv(conn) end)
     listen(socket)
   end
 
-  defp server(conn) do
+  defp recv(conn) do
     case :gen_tcp.recv(conn, 0) do
       {:ok, data} ->
         :gen_tcp.send(conn, data)
-        server(conn)
+        recv(conn)
       {:error, :closed} ->
         :ok
     end
